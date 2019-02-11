0370b821f9b5df30629f517da7bb4106f2c21ba0
Robert Kunze <r.kunze@mobizcorp.de>
2019-02-11 10:31:18 +0100

    chore: adds missing compiled index.js

--- index.js
+++ index.js
@@ -335,18 +335,26 @@ HTMLElement.prototype.addEventListener = function (name, fn) {
 	this._eventListeners[name].push(fn);
 };
 HTMLElement.prototype.dispatchEvent = function (event) {
+	var sendEvent = Object.assign({}, event, {
+		stopPropagation: function stopPropagation() {
+			this.bubbles = false;
+		},
+		preventDefault: function preventDefault() {
+			this.cancelled = true;
+		}
+	});
 	// if we have event listeners registered for the event
-	if (this._eventListeners[event.name]) {
+	if (this._eventListeners[event.type]) {
 		// call them all
-		for (var listener, index = 0, len = this._eventListeners[event.name].length; listener = this._eventListeners[event.name][index], index < len; index += 1) {
-			var result = listener.call(this, event);
-			if (result === false) {
+		for (var listener, index = 0, len = this._eventListeners[event.type].length; listener = this._eventListeners[event.type][index], index < len; index += 1) {
+			var result = listener.call(this, sendEvent);
+			if (result === false || sendEvent.cancelled) {
 				return;
 			}
 		}
 	}
 	// allow the event to bubble up
-	if (this.parentNode) this.parentNode.dispatchEvent(event);
+	if (sendEvent.bubbles && this.parentNode) this.parentNode.dispatchEvent(sendEvent);
 };
 HTMLElement.prototype.removeEventListener = function (name, fn) {
 	if (!fn) {
@@ -356,13 +364,13 @@ HTMLElement.prototype.removeEventListener = function (name, fn) {
 	}
 };
 HTMLElement.prototype.click = function () {
-	this.dispatchEvent({ name: 'click', target: this });
+	this.dispatchEvent({ type: 'click', target: this, bubbles: true });
 };
 HTMLElement.prototype.focus = function () {
-	this.dispatchEvent({ name: 'focus', target: this });
+	this.dispatchEvent({ type: 'focus', target: this, bubbles: true });
 };
 HTMLElement.prototype.blur = function () {
-	this.dispatchEvent({ name: 'blur', target: this });
+	this.dispatchEvent({ type: 'blur', target: this, bubbles: true });
 };
 HTMLElement.prototype.getElementsByTagName = function (tagName) {
 	return findElements(this, function (el) {
